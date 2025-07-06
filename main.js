90d9713cfbe8ff8eaed022123b3930fec7a64dd1
SCM42 <117255815+SCM42@users.noreply.github.com>
2025-07-06 19:19:14 -0400

    Add methods that generate AI and player answers
    Both getComputerChoice() and getHumanChoice() now return either 'Rock',
    'Paper' or 'Scissors' as a result.

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
-console.log("Test");
+console.log(getComputerChoice());
+
+
+//#region
+function getComputerChoice() {
+    let computerResult = "";
+    
+    switch (getRandomNumber(3)) {
+        case 0: 
+            computerResult = "Rock";
+            break;
+        case 1:
+            computerResult = "Paper";
+            break;
+        case 2:
+            computerResult = "Scissors";
+            break;
+        default:
+            console.log("Error: Number out of bonds");
+            break;
+    }
+
+    return computerResult;
+}
+
+function getHumanChoice() {
+    let humanResult = "";
+    
+    switch (getRandomNumber(3)) {
+        case 0: 
+            humanResult = "Rock";
+            break;
+        case 1:
+            humanResult = "Paper";
+            break;
+        case 2:
+            humanResult = "Scissors";
+            break;
+        default:
+            console.log("Error: Number out of bonds");
+            break;
+    }
+
+    return humanResult;
+}
+
+function getRandomNumber(max) {
+    let number = Math.floor(Math.random() * max);
+
+    return number;
+}
+//#endregion
