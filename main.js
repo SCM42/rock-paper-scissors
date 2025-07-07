82c4a6ce60e2860d6eaef8ef32f3a1dc01eea646
SCM42 <117255815+SCM42@users.noreply.github.com>
2025-07-06 20:37:59 -0400

    Create core game logic for playing one round
    Program converts both answers to lowercase and then compares them. If
    they match, it declares a tie and no score change takes place. Otherwise
    , the human answer is fed into a switch-case statement and the win
    condition variable assigned true or false with a conditional statement.
    I created a another function that accepts the win condition as an
    argument and then manages the scores.

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
-console.log(getComputerChoice());
-getHumanChoice();
+var humanScore = 0;
+var computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice)
+    {
+        console.log("It's a tie!");
+    }
+    else {
+        let winCondition = false;
+        
+        switch (humanChoice) {
+            case "rock":
+                winCondition = computerChoice === "scissors" ? true : false;
+                break;
+            case "paper":
+                winCondition = computerChoice === "rock" ? true : false;
+                break;
+            case "scissors":
+                winCondition = computerChoice === "paper" ? true : false;
+                break;
+        }
+
+        changeScore(winCondition);
+    }
+}
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
+
+playRound(humanSelection, computerSelection);
+
+console.log("Player score: " + humanScore);
+console.log("Computer score: " + computerScore);
 
 //#region
 function getComputerChoice() {
     let computerResult = "";
     
+    // assigns value based on number between 0-2
     switch (getRandomNumber(3)) {
         case 0: 
             computerResult = "Rock";
@@ -33,5 +66,16 @@ function getRandomNumber(max) {
     let number = Math.floor(Math.random() * max);
 
     return number;
+}
+
+function changeScore(winCondition) {
+    if (winCondition === true) {
+        console.log("You won!");
+        score++;
+    }
+    else {
+        console.log("You lose.")
+        computerScore++;
+    }
 }
 //#endregion
